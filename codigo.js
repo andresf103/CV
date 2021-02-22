ce7cf1d8be6de40a181c53bf71f4bb76721b505d
andresf103 <andresfernandez103@gmail.com>
2021-02-22 16:12:48 -0300

    un poquitito mejor

--- codigo.js
+++ codigo.js
@@ -2,11 +2,12 @@ let divColapsables = document.getElementsByClassName("colapsable");
 /** */
 for (let i = 0; i < divColapsables.length; i++) {
     divColapsables[i].addEventListener("click", (ev) => {
-        ev.target.classList.toggle("active");
+        ev.target.classList.toggle("active"); //agregamos o quitamos la clase active
         /* nextElementSibling devuelve el elemento inmediato siguiente, en este caso el bloque de texto que esta escondido. */
         let content = ev.target.nextElementSibling;
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
+            /* o no tiene tamaño o le agregamos el tamaño del contenido */
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         }
