0a66ebd90cb136c4fc14a157ab9cfa8bffcb3a56
andresf103 <andresfernandez103@gmail.com>
2021-02-25 10:57:28 -0300

    Finalizado responsive

--- codigo.js
+++ codigo.js
@@ -3,6 +3,8 @@ let divColapsables = document.getElementsByClassName("colapsable");
 for (let i = 0; i < divColapsables.length; i++) {
     divColapsables[i].addEventListener("click", (ev) => {
         ev.target.classList.toggle("active"); //agregamos o quitamos la clase active
+        ev.target.classList.toggle("radioinferior");
+
         /* nextElementSibling devuelve el elemento inmediato siguiente, en este caso el bloque de texto que esta escondido. */
         let content = ev.target.nextElementSibling;
         if (content.style.maxHeight) {
@@ -13,3 +15,4 @@ for (let i = 0; i < divColapsables.length; i++) {
         }
     });
 }
+divColapsables[0].click();
